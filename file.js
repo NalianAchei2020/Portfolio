99f8df63aebd93356bec9e76014e8017d6431168
okari <nyandika15@gmail.com>
2023-06-19 15:55:25 +0300

    Jsfile

--- file.js
+++ file.js
@@ -1,1 +1,11 @@
+const openMenu = document.getElementById("menu");
+const closeMenu = document.getElementById("close_menu");
+const navItems = document.querySelector('.nav-items');
 
+openMenu.addEventListener('click', ()=>{
+ navItems.style.visibility = "visible"
+})
+
+closeMenu.addEventListener('click', ()=>{
+    navItems.style.visibility = "hidden"
+   })
