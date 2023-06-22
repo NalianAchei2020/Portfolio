033abd5f15777d9dc1cd0631ebd6508143f051b2
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-06-22 09:18:10 +0100

    Update file.js

--- file.js
+++ file.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const openMenu = document.getElementById('menu');
 const closeMenu = document.getElementById('close_menu');
 const navItems = document.querySelector('.nav-items');
